a179606678dfb1ce27e8c43586afa7ce4e17a042
Agustin Kassis <webmaster@masize.com>
2025-07-03 18:15:21 -0300

    add lnurl types

---
+++ types/lnurl.ts
@@ -0,0 +1,41 @@
+export interface LnurlRequest {
+  tag: 'withdrawRequest' | 'payRequest'
+  callback: string
+  metadata?: string
+  defaultDescription?: string
+}
+
+export interface LUD03Request extends LnurlRequest {
+  tag: 'withdrawRequest'
+  k1: string
+  minWithdrawable: number
+  maxWithdrawable: number
+  defaultDescription: string
+}
+
+export interface LUD03CallbackSuccess {
+  status: 'OK'
+}
+
+export interface LUD03CallbackError {
+  status: 'ERROR'
+  reason: string
+}
+
+export interface LUD06Response {
+  tag: 'payRequest'
+  callback: string
+  maxSendable: number
+  minSendable: number
+}
+
+export interface LUD06CallbackError {
+  status: 'ERROR'
+  reason: string
+}
+
+export interface LUD06CallbackSuccess {
+  pr: string
+  routes: []
+  verify?: string
+}
