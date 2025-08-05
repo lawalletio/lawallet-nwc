9c6fcb3120b9e2b770bb1be2948d23214af95232
Agustin Kassis <webmaster@masize.com>
2025-08-04 21:01:44 -0300

    fix window.nostr type

--- types/nostr.d.ts
+++ types/nostr.d.ts
@@ -2,7 +2,7 @@ declare global {
   interface Window {
     nostr?: {
       getPublicKey: () => Promise<string>
-      signEvent: (event: NostrEvent) => Promise<string>
+      signEvent: (event: NostrEvent) => Promise<NostrEvent>
       nip04: {
         encrypt: (pubkey: string, plaintext: string) => Promise<string>
         decrypt: (pubkey: string, ciphertext: string) => Promise<string>
