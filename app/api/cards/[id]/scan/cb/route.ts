0765fe003f358b83ba4b33e125f53411b1a069b6
Agustin Kassis <webmaster@masize.com>
2025-09-01 15:32:01 -0300

    implement ntag424 callback

--- app/api/cards/[id]/scan/cb/route.ts
+++ app/api/cards/[id]/scan/cb/route.ts
@@ -3,6 +3,7 @@ import { NextRequest, NextResponse } from 'next/server'
 import { prisma } from '@/lib/prisma'
 import { LUD03CallbackSuccess } from '@/types/lnurl'
 import { LN } from '@getalby/sdk'
+import { consumeNtag424FromPC } from '@/lib/ntag424'
 
 // NWC URI will be fetched from the user record
 
@@ -24,14 +25,28 @@ export async function GET(
   // Get query parameters
   const searchParams = req.nextUrl.searchParams
   const pr = searchParams.get('pr') || ''
-  const k = searchParams.get('k') || ''
+  const p = searchParams.get('p') || ''
+  const c = searchParams.get('c') || ''
+
+  if (!p || !c) {
+    return NextResponse.json(
+      { status: 'ERROR', reason: 'Missing required parameters: p and c' },
+      { headers: { 'Access-Control-Allow-Origin': '*' } }
+    )
+  }
+
+  // if (!pr) {
+  //   return NextResponse.json(
+  //     { status: 'ERROR', reason: 'Missing required parameter: pr' },
+  //     { headers: { 'Access-Control-Allow-Origin': '*' } }
+  //   )
+  // }
 
   try {
     // Find card by id in database
     const card = await prisma.card.findUnique({
       where: { id: cardId },
       include: {
-        design: true,
         ntag424: true,
         user: true
       }
@@ -44,6 +59,28 @@ export async function GET(
       )
     }
 
+    const ntag424Response = await consumeNtag424FromPC(card!.ntag424!, p, c)
+
+    if ('error' in ntag424Response) {
+      return NextResponse.json(
+        { status: 'ERROR', reason: ntag424Response.error },
+        { headers: { 'Access-Control-Allow-Origin': '*' } }
+      )
+    }
+
+    // Update lastUsedAt timestamp and ntag.ctr
+    await prisma.card.update({
+      where: { id: cardId },
+      data: {
+        lastUsedAt: new Date(),
+        ntag424: {
+          update: {
+            ctr: ntag424Response.ctrNew
+          }
+        }
+      }
+    })
+
     // Check if it has nwc set up
     if (!card.user?.nwc) {
       return NextResponse.json(
@@ -52,44 +89,33 @@ export async function GET(
       )
     }
 
-    // Update lastUsedAt timestamp
-    await prisma.card.update({
-      where: { id: cardId },
-      data: { lastUsedAt: new Date() }
-    })
+    // Get NWC URI from the user
+    if (!card.user?.nwc) {
+      console.error('User NWC not configured for card:', cardId)
+      return NextResponse.json(
+        {
+          status: 'ERROR',
+          reason: 'User payment service not configured'
+        },
+        { status: 500, headers: { 'Access-Control-Allow-Origin': '*' } }
+      )
+    }
 
-    // Handle the payment if we have a payment request
-    if (pr) {
-      // Get NWC URI from the user
-      if (!card.user?.nwc) {
-        console.error('User NWC not configured for card:', cardId)
-        return NextResponse.json(
-          {
-            status: 'ERROR',
-            reason: 'User payment service not configured'
-          },
-          { status: 500, headers: { 'Access-Control-Allow-Origin': '*' } }
-        )
-      }
-
-      try {
-        const ln = new LN(card.user.nwc)
-        console.log('Processing payment request:', pr)
-        const payment = await ln.pay(pr)
-        console.log('Payment successful:', payment)
-      } catch (error) {
-        console.error('Payment failed:', error)
-        return NextResponse.json(
-          {
-            status: 'ERROR',
-            reason:
-              error instanceof Error
-                ? error.message
-                : 'Payment processing failed'
-          },
-          { status: 500, headers: { 'Access-Control-Allow-Origin': '*' } }
-        )
-      }
+    try {
+      const ln = new LN(card.user.nwc)
+      console.log('Processing payment request:', pr)
+      const payment = await ln.pay(pr)
+      console.log('Payment successful:', payment)
+    } catch (error) {
+      console.error('Payment failed:', error)
+      return NextResponse.json(
+        {
+          status: 'ERROR',
+          reason:
+            error instanceof Error ? error.message : 'Payment processing failed'
+        },
+        { status: 500, headers: { 'Access-Control-Allow-Origin': '*' } }
+      )
     }
 
     return NextResponse.json(
