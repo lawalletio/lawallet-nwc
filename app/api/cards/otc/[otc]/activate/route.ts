9548f7ed038761197b4b7c4a33cfd4af34c56f2b
Agustin Kassis <webmaster@masize.com>
2025-09-01 18:28:04 -0300

    add albyhub subaccount creation

--- app/api/cards/otc/[otc]/activate/route.ts
+++ app/api/cards/otc/[otc]/activate/route.ts
@@ -2,6 +2,12 @@ import { NextResponse } from 'next/server'
 import { prisma } from '@/lib/prisma'
 import { randomUUID } from 'crypto'
 import { validateNip98 } from '@/lib/nip98'
+import { AlbyHub } from '@/lib/albyhub'
+
+const ALBY_API_URL = process.env.ALBY_API_URL!
+const ALBY_BEARER_TOKEN = process.env.ALBY_BEARER_TOKEN!
+const AUTO_GENERATE_ALBY_SUBACCOUNTS =
+  process.env.AUTO_GENERATE_ALBY_SUBACCOUNTS === 'true'
 
 export async function POST(
   request: Request,
@@ -22,7 +28,8 @@ export async function POST(
     const existingUser = await prisma.user.findUnique({
       where: { pubkey },
       include: {
-        lightningAddress: true
+        lightningAddress: true,
+        albySubAccount: true
       }
     })
 
@@ -32,14 +39,33 @@ export async function POST(
     } else {
       // Create new user
       const userId = randomUUID()
+
+      const albyHub = new AlbyHub(ALBY_API_URL, ALBY_BEARER_TOKEN)
+
+      const subAccount = AUTO_GENERATE_ALBY_SUBACCOUNTS
+        ? await albyHub.createSubAccount(`LaWallet-${userId}`)
+        : null
+
       user = await prisma.user.create({
         data: {
           id: userId,
           pubkey,
-          createdAt: new Date()
+          createdAt: new Date(),
+          albyEnabled: !!subAccount,
+          albySubAccount: subAccount
+            ? {
+                create: {
+                  appId: subAccount.id,
+                  nwcUri: subAccount.pairingUri,
+                  username: subAccount.lud16,
+                  nostrPubkey: subAccount.walletPubkey
+                }
+              }
+            : undefined
         },
         include: {
-          lightningAddress: true
+          lightningAddress: true,
+          albySubAccount: true
         }
       })
     }
@@ -64,8 +90,15 @@ export async function POST(
 
     return NextResponse.json({
       userId: user.id,
-      lightningAddress: user.lightningAddress?.username || null
-      //   nwcString: ""
+      lightningAddress: user.lightningAddress?.username || null,
+      albySubAccount: user.albySubAccount
+        ? {
+            appId: user.albySubAccount.appId,
+            nwcUri: user.albySubAccount.nwcUri,
+            username: user.albySubAccount.username
+          }
+        : null,
+      nwcString: user.albySubAccount ? user.albySubAccount.nwcUri : ''
     })
   } catch (error) {
     console.error('Error creating user:', error)
