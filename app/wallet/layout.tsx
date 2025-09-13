d4748a8e53e784f85fcfa7602ba81ae2be67709c
Agustin Kassis <webmaster@masize.com>
2025-09-13 17:10:14 -0300

    add settings provider in wallet

--- app/wallet/layout.tsx
+++ app/wallet/layout.tsx
@@ -5,6 +5,7 @@ import type React from 'react'
 import { WalletProvider } from '@/providers/wallet'
 import { APIProvider } from '@/providers/api'
 import { Toaster } from '@/components/ui/toaster'
+import { SettingsProvider } from '@/providers/settings'
 
 export default function WalletLayout({
   children
@@ -13,10 +14,12 @@ export default function WalletLayout({
 }) {
   return (
     <APIProvider>
-      <WalletProvider>
-        <Toaster />
-        {children}
-      </WalletProvider>
+      <SettingsProvider>
+        <WalletProvider>
+          <Toaster />
+          {children}
+        </WalletProvider>
+      </SettingsProvider>
     </APIProvider>
   )
 }
